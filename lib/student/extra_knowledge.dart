bfc52d776c34a9b0841a0b863358e2d8a92512ce
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2025-02-14 11:49:26 +0530

    first commit

--- lib/student/extra_knowledge.dart
+++ lib/student/extra_knowledge.dart
@@ -976,7 +976,7 @@ class SubCategoryList extends StatelessWidget {
         }
 
         if (snapshot.hasError) {
-          return Center(child: Text("Error: ${snapshot.error}"));
+          return const Center(child: Text("No subcategories found."));
         }
 
         if (!snapshot.hasData || snapshot.data!.isEmpty) {
